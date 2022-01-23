80529e26c5125fe21487d848979de7a36b665b5e
Paul V <pvigar@gmail.com>
2022-01-22 19:38:15 -0500

    added state

--- contactus-page/src/components/Contactusform.js
+++ contactus-page/src/components/Contactusform.js
@@ -1,21 +1,60 @@
+import {useState, useEffect} from "react";
+import axios from "axios"
 
+const initialFormData = {
+    name: "",
+    email: "",
+    birthdate: "",
+    consent: ""
+}
+const initialFormErrors = {
+    name: "",
+    email: "",
+    birthdate: "",
+    consent: ""
+}
 
 function Contactusform() {
+    const [formData, setFormData] = useState(initialFormData)
+    const [formErrors, setFormErrors] = useState(initialFormErrors)
+    const [disabled, setDisabled] = useState(true)
+    const postData = (formData) => {
 
+    }
+    const onChange = (evt) => {
+        const { name, value, type, checked } = evt.target;
+        const valueToUse = type === "checkbox" ? checked : value;
+        change(name, valueToUse);
     return (
         
             <form>
                 <label> Name
-                    <input type="text"/>
+                    <input 
+                    type="text" 
+                    name="name"
+                    onChange={onChange}
+                    value= {formData.name} />
                 </label>
                 <label>Email
-                    <input type="email"/>
+                    <input 
+                    type="email"
+                    name="email"
+                    onChange={onChange}
+                    value={formData.email}
+                    />
                 </label>
                 <label>Birthdate
-                    <input type="date"/>
+                    <input type="date"
+                    name="email"
+                    onChange={onChange}
+                    value={formData.email}/>
                 </label>
                 <label>
-                    <input type="checkbox" id="emailConsent" name="emailConsent"/>
+                    <input type="checkbox" 
+                    id="emailConsent" 
+                    name="consent"
+                    checked = {formData.consent}
+                    onChange={onChange}/>
                     I agree to be contacted by email
                 </label>
                 <button type="button">Clear</button>
